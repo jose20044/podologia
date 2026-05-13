dcc20d42a3769470f546c55c3df38d32271a6e3d
jose20044 <139297133+jose20044@users.noreply.github.com>
2026-05-13 00:51:59 -0400

    Update api-client.js

--- api-client.js
+++ api-client.js
@@ -3,7 +3,7 @@
  * Funciones auxiliares para interactuar con la API
  */
 
-const API_URL = '';
+const API_URL = 'https://podologia-production-0157.up.railway.app';
 
 class PodoClinicAPI {
   constructor() {
